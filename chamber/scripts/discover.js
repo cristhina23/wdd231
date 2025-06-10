7a8f3b230f9ca55b3812ec49f0d94eca3444c0ce
Cristhina <viszellechacon@gmail.com>
2025-06-10 14:01:28 -0500

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -13,7 +13,7 @@ function displayBusinesses() {
         <div class="card-content">
           <div class="card-image">
           <figure>
-          <img src="${business.img}" alt="local shop" />
+          <img src="${business.img}" loading="lazy" alt="local shop" />
           </figure>
         </div>
         <div class="card-info">
@@ -27,9 +27,23 @@ function displayBusinesses() {
         <button>Learn More</button>
       </div>
     `;
+
     container.innerHTML += cardContent;
-    cards.appendChild(container);
+    
   })
+  cards.appendChild(container);
 }
 
 displayBusinesses();
+
+const message = document.querySelector("#visits");
+let lastVisit = localStorage.getItem("lastVisit");
+
+if (lastVisit) {
+  const daysSince = Math.floor((Date.now() - lastVisit) / 86400000);
+  message.textContent = `Your last visit was ${daysSince} days ago.`;
+} else {
+  message.textContent = "Welcome! This is your first visit.";
+}
+
+localStorage.setItem("lastVisit", Date.now());  
